a094934d19fbc6a81b21b046051d57950370770c
chqbge <413276727@qq.com>
2019-08-05 21:01:33 +0800

    1

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -25,5 +25,5 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts')
-    updated_by = models.ForeignKey(User, related_name='+')
+    updated_by = models.ForeignKey(User, null=True, related_name='+')
 
